190806760808d117418dd1a5270aad5416283be2
Ljulienng <ljuliennguyen@gmail.com>
2021-03-11 19:41:10 +0100

    working on vm but not guacamole

--- philo_two/srcs/main.c
+++ philo_two/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 14:53:50 by user42            #+#    #+#             */
-/*   Updated: 2021/03/11 18:07:05 by user42           ###   ########.fr       */
+/*   Updated: 2021/03/11 19:33:52 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,8 +89,6 @@ void		init_philosophers(t_philo *philo, char **av, int x)
 	i = 0;
 	sem_unlink("msg");
 	msg = sem_open("msg", O_CREAT, S_IRWXU, 1);
-	if (philo[0].eat_count == -1)
-		tid = NULL;
 	while (i < ft_atoi(av[1]))
 	{
 		init2(philo, av, i);
